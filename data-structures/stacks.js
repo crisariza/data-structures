df79f48e33ab6cd5581d2615ae7530a335173840
Cristian Ariza <mecrisariza@gmail.com>
2022-06-15 10:33:18 -0300

    Update stacks.js

--- data-structures/stacks.js
+++ data-structures/stacks.js
@@ -2,19 +2,19 @@ class Stack {
   constructor() {
     this.stack = []
   }
-  pop() { // Removes the top item from the stack
+  pop() { // Remove the top item from the stack
     this.stack.pop()
   } 
-  push(item) { // Adds an item to the top of the stack
+  push(item) { // Add an item to the top of the stack
     this.stack.push(item)
   } 
-  peek() { // Returns the item at the top of the stack (but does not remove it)
+  peek() { // Return the item at the top of the stack (but do not remove it)
     return this.stack[this.length - 1]
   }
-  isEmpty() { // Returns true if the stack is empty
+  isEmpty() { // Return true if the stack is empty
     return this.length === 0
   }
-	get length() { // Returns the number of items in the stack
+	get length() { // Return the number of items in the stack
     return this.stack.length
   } 
 }
